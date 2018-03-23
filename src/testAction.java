913d0fb12ba46e615058d36809202429af0cf70f
刘建广 <xxx@xxx.xxx>
2018-03-23 17:43:06 +0800

    asdf

--- src/testAction.java
+++ src/testAction.java
@@ -4,7 +4,7 @@ public class testAction {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("HELLO1");
-         System.out.println("哈哈哈哈原来这么容易");
+         System.out.println("哈哈哈哈原来这么容易 develop");
 	}
 
 }
